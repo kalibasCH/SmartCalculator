a4a171101fbae5b3f78f0b3a8520d3db6dfb8f7a
Nikolay Chebotarev <kalibas1@mail.ru>
2023-01-29 17:15:12 +0300

    stage 4 is complite

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -1,9 +1,7 @@
-package calculator
-
 fun main() {
     var statusToExit = false
     while (!statusToExit) {
-        when(val inputData = Bot().readData()) {
+        when (val inputData = Bot().readData()) {
             "/exit" -> {
                 statusToExit = true
                 Bot().sayBye()
@@ -11,7 +9,7 @@ fun main() {
             "" -> continue
             "/help" -> Bot().sayHelp()
             else -> {
-                val sumOfInteger = SumOfInteger().returnSumOfInteger(inputData)
+                val sumOfInteger = Calculator().performCalculation(inputData)
                 println(sumOfInteger)
             }
         }
@@ -21,16 +19,62 @@ fun main() {
 class Bot {
     fun readData() = readln()
     fun sayBye() = println("Bye!")
-    fun sayHelp() = println("The program calculates the sum of numbers")
+    fun sayHelp() = println(
+        """It means that from now on the program must receive the addition + 
+        |and subtraction - operators as an input to distinguish operations from each other. 
+        |It must support both unary and binary minus operators. Moreover, If the user has entered 
+        |several same operators following each other, the program still should work 
+        |(like Java or Python REPL). Also, as you remember from school math, two adjacent minus 
+        |signs turn into a plus. Therefore, if the user inputs --, it should be read as +; if they 
+        |input ----, it should be read as ++, and so on. The smart calculator ought to have such 
+        |a feature.""".trimMargin()
+    )
 }
 
-class SumOfInteger {
-    fun returnSumOfInteger(inputStringData: String): Int {
+class Calculator {
+
+    fun performCalculation(inputStringData: String): Int {
         val listOfIntegerAfterCheckFormat = checkListIntegerFormat(inputStringData)
         return listOfIntegerAfterCheckFormat.sum()
     }
 
     private fun checkListIntegerFormat(inputStringData: String): List<Int> {
-        return inputStringData.split(" ").map { it.toInt() }
+        val inputListOfInt = inputStringData.split(" ").map { it }.filter { it != "+" }
+        var elementsOfCalculation = inputListOfInt.toMutableList()
+        for (element in inputListOfInt) {
+            val indexOfElementInList = elementsOfCalculation.indexOf(element)
+            when {
+                element == "-" -> elementsOfCalculation = multiplyNextIntByMinusOne(elementsOfCalculation)
+                checkThatItInt(element) -> continue
+                else -> {
+                    if (element.length % 2 == 0 && element[0] == '-') {
+                        elementsOfCalculation.removeAt(indexOfElementInList)
+                    } else if (element.length % 2 == 1 && element[0] == '-') {
+                        elementsOfCalculation[indexOfElementInList + 1] =
+                            (elementsOfCalculation[indexOfElementInList + 1].toInt() * -1).toString()
+                        elementsOfCalculation.removeAt(indexOfElementInList)
+                    } else {
+                        elementsOfCalculation.removeAt(indexOfElementInList)
+                    }
+
+                }
+            }
+        }
+        return elementsOfCalculation.map { it.toInt() }
+    }
+
+    private fun multiplyNextIntByMinusOne(listOfElements: MutableList<String>): MutableList<String> {
+        listOfElements[listOfElements.indexOf("-") + 1] =
+            (listOfElements[listOfElements.indexOf("-") + 1].toInt() * -1).toString()
+        listOfElements.removeAt(listOfElements.indexOf("-"))
+        return listOfElements
+    }
+
+    private fun checkThatItInt(element: String): Boolean {
+        return try {
+            element.toInt() in (Int.MIN_VALUE..Int.MAX_VALUE)
+        } catch (e: Exception) {
+            false
+        }
     }
 }
