cea794038ad1e7a70c6914ff78a33f3615414403
Nikolay Chebotarev <kalibas1@mail.ru>
2023-01-29 13:35:43 +0300

    stage 2 is complite

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -1,6 +1,34 @@
 package calculator
 
 fun main() {
-    val listOfInt = readln().split(" ").map { it.toInt() }
-    println(listOfInt.sum())
+    var statusToExit = false
+    while (!statusToExit) {
+        when(val inputData = Bot().readData()) {
+            "/exit" -> {
+                statusToExit = true
+                Bot().sayBye()
+            }
+            "" -> continue
+            else -> {
+                val sumOfInteger = SumOfInteger().returnSumOfInteger(inputData)
+                println(sumOfInteger)
+            }
+        }
+    }
+}
+
+class Bot {
+    fun readData() = readln()
+    fun sayBye() = println("Bye!")
+}
+
+class SumOfInteger {
+    fun returnSumOfInteger(inputStringData: String): Int {
+        val listOfIntegerAfterCheckFormat = checkListIntegerFormat(inputStringData)
+        return listOfIntegerAfterCheckFormat.sum()
+    }
+
+    private fun checkListIntegerFormat(inputStringData: String): List<Int> {
+        return inputStringData.split(" ").map { it.toInt() }
+    }
 }
